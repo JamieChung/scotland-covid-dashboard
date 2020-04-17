5a7143bea6abe1eb55f1629196005302fae4889b
Jamie Chung <me@jamiechung.me>
2020-04-17 07:32:23 +0100

    using next/head

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import ReactGA from 'react-ga';
 import App from './components/app';
-
+import Head from 'next/head';
 export default class HomePage extends React.Component {
 
     componentDidMount() {
@@ -14,29 +14,26 @@ export default class HomePage extends React.Component {
 
     render() {
         return (
-            <html>
-                <head>
+            <div>
+                <Head>
                     <title>Scotland COVID Dashboard</title>
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
                     <meta name="description" content="Scotland COVID Dashboard tracking daily COVID-19 cases." />
-                    <meta name="viewport" content="width=device-width, initial-scale=1" />
-                </head>
-                <body>
-                   <App /> 
-                    <Container style={{ marginTop: 50, marginBottom: 50 }}>
-                        <Divider variant="middle" />
-                        <Typography variant="body1" style={{ textAlign: 'center', marginTop: 25 }}>
-                            <ReactGA.OutboundLink eventLabel="outbound-github.com/jamiechung/scotland-covid-dashboard" target="_blank" to="https://github.com/JamieChung/scotland-covid-dashboard">
-                                Contribute on GitHub
+                </Head>
+                <App />
+                <Container style={{ marginTop: 50, marginBottom: 50 }}>
+                    <Divider variant="middle" />
+                    <Typography variant="body1" style={{ textAlign: 'center', marginTop: 25 }}>
+                        <ReactGA.OutboundLink eventLabel="outbound-github.com/jamiechung/scotland-covid-dashboard" target="_blank" to="https://github.com/JamieChung/scotland-covid-dashboard">
+                            Contribute on GitHub
                         </ReactGA.OutboundLink>
-                            <span> - Made with ❤️ in Edinburgh, Scotland - </span>
-                            <ReactGA.OutboundLink eventLabel="outbound-github.com/tomwhite/covid-19-uk-data" target="_blank" to="https://github.com/tomwhite/covid-19-uk-data">
-                                Data provided by Tom White
+                        <span> - Made with ❤️ in Edinburgh, Scotland - </span>
+                        <ReactGA.OutboundLink eventLabel="outbound-github.com/tomwhite/covid-19-uk-data" target="_blank" to="https://github.com/tomwhite/covid-19-uk-data">
+                            Data provided by Tom White
                         </ReactGA.OutboundLink>
-                        </Typography>
-                    </Container>
-                </body>
-            </html>
+                    </Typography>
+                </Container>
+            </div>
         );
     }
 }
