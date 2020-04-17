235299af6cfb912c7c402132955382b26111b65a
Jamie Chung <me@jamiechung.me>
2020-04-17 06:49:59 +0100

    small changes

--- pages/components/app.tsx
+++ pages/components/app.tsx
@@ -1,4 +1,4 @@
-import { Divider, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
@@ -11,42 +11,43 @@ import TopCountriesTable from '../components/topCountriesTable';
 
 export default class App extends React.Component {
     render() {
-        return (<Container>
-            <Typography variant="h4">Scotland COVID-19 Dashboard</Typography>
-            <Typography variant="overline" style={{ marginBottom: 25, display: 'block' }}>Updated Daily - BST/GMT</Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12} md={6}>
-                    <SingleKPICard title="Total number of confirmed cases in Scotland" query={queries.QUERY_TOTAL_CONFIRMED_CASES_SCOTLAND} />
+        return (
+            <Container>
+                <Typography variant="h4">Scotland COVID-19 Dashboard</Typography>
+                <Typography variant="overline" style={{ marginBottom: 25, display: 'block' }}>Updated Daily - BST/GMT</Typography>
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={6}>
+                        <SingleKPICard title="Total number of confirmed cases in Scotland" query={queries.QUERY_TOTAL_CONFIRMED_CASES_SCOTLAND} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <SingleKPICard title="Latest daily number of confirmed cases in Scotland" query={queries.QUERY_DAILY_CHANGE_CONFIRMED_CASES_SCOTLAND} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <SingleKPICard title="Total number of tests in Scotland" query={queries.QUERY_TOTAL_TESTS_SCOTLAND} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <SingleKPICard title="Latest daily number of tests in Scotland" query={queries.QUERY_DAILY_CHANGE_TESTS_SCOTLAND} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <SingleKPICard title="Total number of deaths in Scotland" query={queries.QUERY_TOTAL_DEATHS_SCOTLAND} />
+                    </Grid>
+                    <Grid item xs={12} md={6}>
+                        <SingleKPICard title="Latest daily number of deaths in Scotland" query={queries.QUERY_DAILY_CHANGE_DEATHS_SCOTLAND} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <CountryCharts />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TopAreasTable />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TopCountriesTable />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <AreasCharts />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <SingleKPICard title="Latest daily number of confirmed cases in Scotland" query={queries.QUERY_DAILY_CHANGE_CONFIRMED_CASES_SCOTLAND} />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <SingleKPICard title="Total number of tests in Scotland" query={queries.QUERY_TOTAL_TESTS_SCOTLAND} />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <SingleKPICard title="Latest daily number of tests in Scotland" query={queries.QUERY_DAILY_CHANGE_TESTS_SCOTLAND} />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <SingleKPICard title="Total number of deaths in Scotland" query={queries.QUERY_TOTAL_DEATHS_SCOTLAND} />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <SingleKPICard title="Latest daily number of deaths in Scotland" query={queries.QUERY_DAILY_CHANGE_DEATHS_SCOTLAND} />
-                </Grid>
-                <Grid item xs={12}>
-                    <CountryCharts />
-                </Grid>
-                <Grid item xs={12}>
-                    <TopAreasTable />
-                </Grid>
-                <Grid item xs={12}>
-                    <TopCountriesTable />
-                </Grid>
-                <Grid item xs={12}>
-                    <AreasCharts />
-                </Grid>
-            </Grid>
-        </Container>
+            </Container>
         );
     }
 }
